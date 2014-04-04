567102f5529f69c1fd5b1254b26ac16f41a4c59e
Oliver Letterer <oliver.letterer@gmail.com>
2014-04-04 09:29:25 +0200

    Release 1.1.3.

--- SLObjectiveCRuntimeAdditions/SLObjectiveCRuntimeAdditions.m
+++ SLObjectiveCRuntimeAdditions/SLObjectiveCRuntimeAdditions.m
@@ -26,7 +26,7 @@
         _object = object;
         _class = class;
     }
-    
+
     return self;
 }
 
@@ -34,10 +34,10 @@
 {
     NSParameterAssert(selector);
     NSParameterAssert(blockImplementation);
-    
+
     NSAssert([[self.object class] instancesRespondToSelector:selector], @"Object does not respond to selector: %@", NSStringFromSelector(selector));
     Method method = class_getInstanceMethod(object_getClass(self.object), selector);
-    
+
     return [self implementInstanceMethodNamed:selector types:method_getTypeEncoding(method) implementation:blockImplementation];
 }
 
@@ -46,12 +46,12 @@
     NSParameterAssert(selector);
     NSParameterAssert(blockImplementation);
     NSParameterAssert(types);
-    
+
     SLBlockDescription *blockDescription = [[SLBlockDescription alloc] initWithBlock:blockImplementation];
     NSMethodSignature *methodSignature = [NSMethodSignature signatureWithObjCTypes:types];
-    
+
     NSAssert([blockDescription blockSignatureIsCompatibleWithMethodSignature:methodSignature], @"block for %@ is not compatible", NSStringFromSelector(selector));
-    
+
     IMP implementation = imp_implementationWithBlock(blockImplementation);
     return class_addMethod(self.class, selector, implementation, types);
 }
@@ -76,26 +76,26 @@ void class_swizzlesMethodsWithPrefix(Class class, NSString *prefix)
     SLMethodEnumertor enumeratorBlock = ^(Class class, Method method) {
         SEL methodSelector = method_getName(method);
         NSString *selectorString = NSStringFromSelector(methodSelector);
-        
+
         if ([selectorString hasPrefix:prefix]) {
             NSMutableString *originalSelectorString = [selectorString stringByReplacingOccurrencesOfString:prefix withString:@"" options:NSLiteralSearch range:NSMakeRange(0, prefix.length)].mutableCopy;
-            
+
             if (originalSelectorString.length > 0) {
                 NSString *uppercaseFirstCharacter = [originalSelectorString substringToIndex:1];
                 NSString *lowercaseFirstCharacter = uppercaseFirstCharacter.lowercaseString;
-                
+
                 [originalSelectorString replaceCharactersInRange:NSMakeRange(0, 1) withString:lowercaseFirstCharacter];
-                
+
                 SEL originalSelector = NSSelectorFromString(originalSelectorString);
-                
+
                 class_swizzleSelector(class, originalSelector, methodSelector);
             }
         }
     };
-    
+
     // swizzle instance methods
     class_enumerateMethodList(class, enumeratorBlock);
-    
+
     // swizzle class methods
     Class metaClass = objc_getMetaClass(class_getName(class));
     class_enumerateMethodList(metaClass, enumeratorBlock);
@@ -104,49 +104,49 @@ void class_swizzlesMethodsWithPrefix(Class class, NSString *prefix)
 void class_enumerateMethodList(Class class, SLMethodEnumertor enumerator)
 {
     if (!enumerator) return;
-    
+
     static dispatch_queue_t queue = NULL;
-    
+
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         queue = dispatch_queue_create("de.ebf.objc_runtime_additions.method_enumeration_queue", DISPATCH_QUEUE_CONCURRENT);
     });
-    
+
     unsigned int methodCount = 0;
     Method *methods = class_copyMethodList(class, &methodCount);
-    
+
     dispatch_apply(methodCount, queue, ^(size_t index) {
         Method method = methods[index];
         enumerator(class, method);
     });
-    
+
     free(methods);
 }
 
 Class class_subclassPassingTest(Class class, SLClassTest test)
 {
     if (!test) return nil;
-    
+
     static dispatch_queue_t queue = NULL;
-    
+
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         queue = dispatch_queue_create("de.ebf.objc_runtime_additions.class_queue", DISPATCH_QUEUE_CONCURRENT);
     });
-    
+
     unsigned int numberOfClasses = 0;
     Class *classList = objc_copyClassList(&numberOfClasses);
-    
+
     __block Class testPassingClass = nil;
-    
+
     dispatch_apply(numberOfClasses, queue, ^(size_t classIndex) {
         if (testPassingClass != nil) {
             return;
         }
-        
+
         Class thisClass = classList[classIndex];
         Class superClass = thisClass;
-        
+
         while ((superClass = class_getSuperclass(superClass))) {
             if (superClass == class || thisClass == class) {
                 if (test(thisClass)) {
@@ -155,17 +155,17 @@ Class class_subclassPassingTest(Class class, SLClassTest test)
             }
         }
     });
-    
+
     // cleanup
     free(classList);
-    
+
     return testPassingClass;
 }
 
 IMP class_replaceMethodWithBlock(Class class, SEL originalSelector, id block)
 {
     IMP newImplementation = imp_implementationWithBlock(block);
-    
+
     Method method = class_getInstanceMethod(class, originalSelector);
     return class_replaceMethod(class, originalSelector, newImplementation, method_getTypeEncoding(method));
 }
@@ -173,30 +173,30 @@ IMP class_replaceMethodWithBlock(Class class, SEL originalSelector, id block)
 void class_implementPropertyInUserDefaults(Class class, NSString *propertyName, BOOL automaticSynchronizeUserDefaults)
 {
     NSString *userDefaultsKey = [NSString stringWithFormat:@"%@__%@", NSStringFromClass(class), propertyName];
-    
+
     SEL getter = NSSelectorFromString(propertyName);
     NSString *firstLetter = [propertyName substringToIndex:1];
     NSString *setterName = [propertyName stringByReplacingCharactersInRange:NSMakeRange(0, 1) withString:[NSString stringWithFormat:@"set%@", firstLetter.uppercaseString]];
     setterName = [setterName stringByAppendingString:@":"];
     SEL setter = NSSelectorFromString(setterName);
-    
+
     IMP getterImplementation = imp_implementationWithBlock(^id(id self) {
         // 1) try to read from cache
         return [[NSUserDefaults standardUserDefaults] objectForKey:userDefaultsKey];
     });
-    
+
     IMP setterImplementation = imp_implementationWithBlock(^(id self, id object) {
         if (!object) {
             [[NSUserDefaults standardUserDefaults] removeObjectForKey:userDefaultsKey];
         } else {
             [[NSUserDefaults standardUserDefaults] setObject:object forKey:userDefaultsKey];
-            
+
             if (automaticSynchronizeUserDefaults) {
                 [[NSUserDefaults standardUserDefaults] synchronize];
             }
         }
     });
-    
+
     class_addMethod(class, getter, getterImplementation, "@@:");
     class_addMethod(class, setter, setterImplementation, "v@:@");
 }
@@ -205,28 +205,28 @@ void class_implementProperty(Class class, NSString *propertyName)
 {
     NSCAssert(class != Nil, @"class is required");
     NSCAssert(propertyName != nil, @"propertyName is required");
-    
+
     objc_property_t property = class_getProperty(class, propertyName.UTF8String);
-    
+
     unsigned int count = 0;
     objc_property_attribute_t *attributes = property_copyAttributeList(property, &count);
-    
+
     typedef enum {
         MemoryManagementAssign,
         MemoryManagementCopy,
         MemoryManagementRetain
     } MemoryManagement;
-    
+
     MemoryManagement memoryManagement = MemoryManagementAssign;
     BOOL isNonatomic = NO;
-    
+
     NSString *getterName = nil;
     NSString *setterName = nil;
     NSString *encoding = nil;
-    
+
     for (int i = 0; i < count; i++) {
         objc_property_attribute_t attribute = attributes[i];
-        
+
         switch (attribute.name[0]) {
             case 'N':
                 isNonatomic = YES;
@@ -253,51 +253,51 @@ void class_implementProperty(Class class, NSString *propertyName)
                 break;
         }
     }
-    
+
     free(attributes);
-    
+
     if (!getterName) {
         getterName = propertyName;
     }
-    
+
     if (!setterName) {
         NSString *firstLetter = [propertyName substringToIndex:1];
         setterName = [propertyName stringByReplacingCharactersInRange:NSMakeRange(0, 1) withString:[NSString stringWithFormat:@"set%@", firstLetter.uppercaseString]];
         setterName = [setterName stringByAppendingString:@":"];
     }
-    
+
     NSCAssert([encoding characterAtIndex:0] != '{', @"structs are not supported");
     NSCAssert([encoding characterAtIndex:0] != '(', @"unions are not supported");
-    
+
     SEL getter = NSSelectorFromString(getterName);
     SEL setter = NSSelectorFromString(setterName);
-    
-    if (encoding.UTF8String[0] == @encode(id)[0]) {
+
+    if ([encoding characterAtIndex:0] == @encode(id)[0]) {
         IMP getterImplementation = imp_implementationWithBlock(^id(id self) {
             return objc_getAssociatedObject(self, getter);
         });
-        
+
         objc_AssociationPolicy associationPolicy = 0;
-        
+
         if (memoryManagement == MemoryManagementCopy) {
             associationPolicy = isNonatomic ? OBJC_ASSOCIATION_COPY_NONATOMIC : OBJC_ASSOCIATION_COPY;
         } else {
             associationPolicy = isNonatomic ? OBJC_ASSOCIATION_RETAIN_NONATOMIC : OBJC_ASSOCIATION_RETAIN;
         }
-        
+
         IMP setterImplementation = imp_implementationWithBlock(^(id self, id object) {
             objc_setAssociatedObject(self, getter, object, associationPolicy);
         });
-        
+
         class_addMethod(class, getter, getterImplementation, "@@:");
         class_addMethod(class, setter, setterImplementation, "v@:@");
-        
+
         return;
     }
-    
+
     objc_AssociationPolicy associationPolicy = isNonatomic ? OBJC_ASSOCIATION_RETAIN_NONATOMIC : OBJC_ASSOCIATION_RETAIN;
-    
-#define CASE(type, selectorpart) if (encoding.UTF8String[0] == @encode(type)[0]) {\
+
+#define CASE(type, selectorpart) if ([encoding characterAtIndex:0] == @encode(type)[0]) {\
 IMP getterImplementation = imp_implementationWithBlock(^type(id self) {\
 return [objc_getAssociatedObject(self, getter) selectorpart##Value];\
 });\
@@ -311,7 +311,7 @@ class_addMethod(class, setter, setterImplementation, "v@:@");\
 \
 return;\
 }
-    
+
     CASE(char, char);
     CASE(unsigned char, unsignedChar);
     CASE(short, short);
@@ -325,9 +325,9 @@ return;\
     CASE(float, float);
     CASE(double, double);
     CASE(BOOL, bool);
-    
+
 #undef CASE
-    
+
     NSCAssert(NO, @"encoding %@ in not supported", encoding);
 }
 
@@ -341,41 +341,41 @@ void __attribute__((overloadable)) object_ensureDynamicSubclass(id object, NSStr
     NSCParameterAssert(object);
     NSCAssert(classSuffix.length > 0, @"Invalid class suffix");
     NSCParameterAssert(constructor);
-    
+
     static OSSpinLock lock = OS_SPINLOCK_INIT;
     OSSpinLockLock(&lock);
-    
+
     Class currentClass = object_getClass(object);
     SEL alreadySubclassedSelector = NSSelectorFromString([NSString stringWithFormat:@"__SLObjcRuntimeAdditions_%@", classSuffix]);
-    
+
     BOOL alreadySubclassed = class_respondsToSelector(currentClass, alreadySubclassedSelector);
     if (alreadySubclassed) {
         OSSpinLockUnlock(&lock);
         return;
     }
-    
+
     NSString *newDynamicSubclassName = [NSString stringWithFormat:@"%@_%@", currentClass, classSuffix];
     Class newDynamicSubclass = NSClassFromString(newDynamicSubclassName);
-    
+
     if (!newDynamicSubclass) {
         newDynamicSubclass = objc_allocateClassPair(currentClass, newDynamicSubclassName.UTF8String, 0);
-        
+
         SLObjectRuntimeDynamicSubclassConstructor *constructorObject = [[SLObjectRuntimeDynamicSubclassConstructor alloc] initWithObject:object class:newDynamicSubclass];
-        
+
         [constructorObject implementInstanceMethodNamed:alreadySubclassedSelector types:"v@:" implementation:^(id self) { }];
-        
+
         if (hideDynamicSubclass) {
             [constructorObject implementInstanceMethodNamed:@selector(class) implementation:^Class(id self) {
                 return class_getSuperclass(object_getClass(self));
             }];
         }
-        
+
         constructor(constructorObject);
-        
+
         objc_registerClassPair(newDynamicSubclass);
     }
-    
+
     object_setClass(object, newDynamicSubclass);
-    
+
     OSSpinLockUnlock(&lock);
 }
